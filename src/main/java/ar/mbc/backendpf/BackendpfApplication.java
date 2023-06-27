b50f036462718fad4dfc7941a347f8f7ec04f37a
Marta Caldentey <martab.caldentey@gmail.com>
2023-06-27 18:34:56 -0300

    commit inicial

---
+++ src/main/java/ar/mbc/backendpf/BackendpfApplication.java
@@ -0,0 +1,13 @@
+package ar.mbc.backendpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendpfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/mbc/backendpf/BackendpfApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.mbc.backendpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
